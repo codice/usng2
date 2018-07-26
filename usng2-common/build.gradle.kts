50a070e5fd81246b90330a2a7b8f7c06186a398d
R.A. Porter <coyotesqrl@gmail.com>
2018-07-26 14:06:13 -0700

    Differentiates licenses between Codice-only and Codice+progenitors of the library. (#1)
    Adds license headers to build files.

--- usng2-common/build.gradle.kts
+++ usng2-common/build.gradle.kts
@@ -1,3 +1,11 @@
+/*
+Copyright (c) 2018 Codice Foundation
+
+Released under The MIT License; see
+http://www.opensource.org/licenses/mit-license.php
+or http://en.wikipedia.org/wiki/MIT_License
+*/
+// Build file
 plugins {
     id("kotlin-platform-common") version "1.2.51"
 }
--- usng2-js/src/main/kotlin/Utm.kt
+++ usng2-js/src/main/kotlin/Utm.kt
@@ -1,8 +1,5 @@
 /*
-Copyright (c) 2009 Larry Moore, larmoor@gmail.com
-              2014 Mike Adair, Richard Greenwood, Didier Richard, Stephen Irons, Olivier Terral and
-                   Calvin Metcalf (proj4js)
-              2018 Codice Foundation
+Copyright (c) 2018 Codice Foundation
 
 Released under The MIT License; see
 http://www.opensource.org/licenses/mit-license.php
